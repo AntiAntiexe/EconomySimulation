0382e9b62a7c615935dc6b8c647d69d6a605a620
AntiAntiexe <mrbricken@gmail.com>
2025-06-28 07:55:49 +1000

    code to generate graphs of the data

---
+++ graph.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+firmData = pd.read_csv('firms.csv')
+householdData = pd.read_csv('households.csv')
+
+figure, axis = plt.subplots(2, 2)
+
+axis[0, 0].plot(firmData['Day'], firmData['Money'], label='Money')
+axis[0, 0].set_title("Firm Money Over Time")
+
+axis[0, 1].plot(firmData['Day'], firmData['Goods'], label='Goods')
+axis[0, 1].set_title("Firm Goods Over Time")
+
+axis[1, 0].plot(firmData['Day'], firmData['GoodsPrice'], label='Goods Price')
+axis[1, 0].set_title("Firm Goods Price Over Time")
+
+axis[1, 1].plot(firmData['Day'], firmData['Land'], label='Land')
+axis[1, 1].plot(firmData['Day'], firmData['Labour'], label='Labour')
+axis[1, 1].plot(firmData['Day'], firmData['Capital'], label='Capital')
+axis[1, 1].set_title("Firm Land Over Time")
+
+axis[1, 1].legend()
+
+plt.show()
