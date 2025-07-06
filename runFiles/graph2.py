44b15c8a3d39e586e817128823270ab454e1a71d
AntiAntiexe <mrbricken@gmail.com>
2025-07-06 17:32:37 +1000

    comment out old non oop verison

--- runFiles/graph2.py
+++ runFiles/graph2.py
@@ -7,18 +7,28 @@ figure, axis = plt.subplots(3)
 
 figure.tight_layout(pad=3.0)
 
-axis[0].plot(householdData['Day'], householdData['Income'], label='Income')
-axis[0].set_title("Household Money Over Time")
+datasets = []
 
-axis[1].plot(householdData['Day'], householdData['Goods'], label='Goods')
-axis[1].set_title("Household Goods Over Time")
+for i in householdData['DataSet'].unique():
+    datasets.append(householdData.loc[householdData['DataSet'] == i])
 
-axis[2].plot(householdData['Day'], householdData['Land'], label='Land')
-axis[2].plot(householdData['Day'], householdData['Labour'], label='Labour')
-axis[2].plot(householdData['Day'], householdData['Capital'], label='Capital')
-axis[2].set_title("Household Resources Over Time")
+for data in datasets:
+    axis[0, 0].plot(data['Day'], data['Income'], label=data['DataSet'].iloc[0])
+    axis[0, 1].plot(data['Day'], data['Goods'], label=data['DataSet'].iloc[0])
+    axis[0, 2].plot(data['Day'], data['Land'], label=data['DataSet'].iloc[0])
+    axis[1, 0].plot(data['Day'], data['Labour'], label='Labour')
+    axis[1, 1].plot(data['Day'], data['Capital'], label=data['DataSet'].iloc[0])
 
-axis[2].legend()
+
+axis[0, 0].set_title("Household Money Over Time")
+axis[0, 1].set_title("Household Goods Over Time")
+axis[0, 2].set_title("Household Land Over Time")
+axis[1, 0].set_title("Household Labour Over Time")
+axis[1, 1].set_title("Household Capital Over Time")  
+
+
+
+
 
 plt.show()
 '''
