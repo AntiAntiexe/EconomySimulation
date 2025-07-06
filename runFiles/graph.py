8c956d4011b85ea7490cacac544c6525e3987829
AntiAntiexe <mrbricken@gmail.com>
2025-07-06 17:31:47 +1000

    add multiple data set functionality

--- runFiles/graph.py
+++ runFiles/graph.py
@@ -4,7 +4,34 @@ import pandas as pd
 firmData = pd.read_csv('data/firms.csv')
 
 
-figure, axis = plt.subplots(2, 2)
+figure, axis = plt.subplots(2, 3)
+plt.tight_layout(pad=3.0)
+
+datasets = []
+
+for i in firmData['DataSet'].unique():
+    datasets.append(firmData.loc[firmData['DataSet'] == i])
+
+for data in datasets:
+    axis[0, 0].plot(data['Day'], data['Money'], label=data['DataSet'].iloc[0])
+    axis[0, 1].plot(data['Day'], data['Goods'], label=data['DataSet'].iloc[0])
+    axis[0, 2].plot(data['Day'], data['GoodsPrice'], label=data['DataSet'].iloc[0])
+    axis[1, 0].plot(data['Day'], data['Land'], label=data['DataSet'].iloc[0])
+    axis[1, 1].plot(data['Day'], data['Labour'], label=data['DataSet'].iloc[0])
+    axis[1, 2].plot(data['Day'], data['Capital'], label=data['DataSet'].iloc[0])
+
+
+axis[0, 0].set_title("Firm Money Over Time")
+axis[0, 1].set_title("Firm Goods Over Time")
+axis[0, 2].set_title("Firm Goods Price Over Time")
+axis[1, 0].set_title("Firm Land Over Time")
+axis[1, 1].set_title("Firm Labour Over Time")
+axis[1, 2].set_title("Firm Capital Over Time")
+
+plt.show()
+
+
+'''
 
 axis[0, 0].plot(firmData['Day'], firmData['Money'], label='Money')
 axis[0, 0].set_title("Firm Money Over Time")
