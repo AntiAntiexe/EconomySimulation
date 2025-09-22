0ee505c704c01a595b13ebee5ee1b1a4c25f8c2b
AntiAntiexe <mrbricken@gmail.com>
2025-09-22 15:29:43 +1000

    Remove commented-out code and improve random household selection logic

--- runFiles/randomApproach.py
+++ runFiles/randomApproach.py
@@ -1,3 +1,21 @@
 import random
+'''
+exclude = []
+for i in range(10):
+    household = random.choice([j for j in range(0,10 - 1) if j not in exclude])
+    exclude.append(household)
+    print(household)
+    
 
-print(random.randint(0, 100))
+
+'''
+
+nhouseholdsList = list(range(0,10-1))
+
+for i in range(10):
+    randHouse = random.choice(nhouseholdsList)
+    print(randHouse)
+    nhouseholdsList.remove(randHouse)
+    print('---')
+    
+    
